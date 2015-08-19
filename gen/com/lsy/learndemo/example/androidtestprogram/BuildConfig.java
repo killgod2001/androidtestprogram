7ff9a6dfa206413d49a9bc5cbc3f83ba748ab2f4
killgod2001 <kill_god_2001@163.com>
2015-08-19 11:34:36 +0800

    today first try.create a android program.

---
+++ gen/com/lsy/learndemo/example/androidtestprogram/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.lsy.learndemo.example.androidtestprogram;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
